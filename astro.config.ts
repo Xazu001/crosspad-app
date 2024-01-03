84fca94b50e3938910421e373e450ea686dbc73c
Xazu <Xazu001@users.noreply.github.com>
2024-01-03 19:32:43 +0100

    test

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from '@astrojs/sitemap';
 
 import react from "@astrojs/react";
 
@@ -11,8 +12,10 @@ export default defineConfig({
     functionPerRoute: true,
     routes: {
       strategy: 'include',
-      include: ['/authors/*'],
+      include: ['/authors/*', '/'],
     },
   }),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  site: 'https://crosspad.app',
+
 });
